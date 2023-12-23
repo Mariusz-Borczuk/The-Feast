35ce7603d875dd04c1fcfcbb2fed635a2455c9b1
Bubbline mint <mariusz.borczuk@edu.uekat.pl>
2023-12-23 19:30:18 +0100

    Update Feast.py with new labels and images

--- Feast.py
+++ Feast.py
@@ -23,25 +23,51 @@ class FeastApp:
 
         # Create a Labels widget
         # 1st label
-        label1 = tk.Label(self.root, text="Welcome to The Feast paragraph!", font=("Lobster", 32))
+        label1 = tk.Label(self.root, text="Welcome to The Feast paragraph game!", font=("Lobster", 32))
         label1.pack(side = "top")
         # 2nd label
-        label2 = tk.Label(self.root, text="- Grandfather Ivan: He is your mother's father, and he is very old and wise.\n He likes to tell stories and legends, and he knows a lot about Slavic culture and traditions. \nHe is also very strict and conservative, \nand often scolds guests who do not follow the rules or respect the customs.", font=("Lobster", 16))
+               
+        label2 = tk.Label(self.root, text="Please meet the family members and get to know them." , font=("Lobster", 16))
         label2.pack()
 
-        # images
-        # 1st image
+        # Images
+        Images =set()
+        for i in range(1, 11):
+            subset_name = f"image{i}"
+            Images.add(subset_name)
+        
+
         image1 = Image.open("Images/pierwsi.png")
         image1 = ImageTk.PhotoImage(image1)
-        label1 = tk.Label(image=image1)     
-        label1.image = image1
-        label1.pack( padx="10", pady="10")
-
-
+        image2 = Image.open("Images/Vasilisa.jpeg")
+        image2 = ImageTk.PhotoImage(image2)
+        image3 = Image.open('Images/Masha.jpeg')
+        image3 = ImageTk.PhotoImage(image3)
+        Imagesz = tk.Label(image=image1)     
+        Imagesz.image = image1
+        Imagesz.pack( padx="10", pady="10")
+        
+        
         # Create buttons
-        ButtonV = Button(self.root, text="Vasilisa", font=("Lobster", 20, 'bold'), bg="red").pack(side="left", padx="10", pady="10")
-        ButtonM = Button(self.root, text="Masha", font=("Lobster", 20, 'bold'), bg="blue").pack(side="right", padx="10", pady="10")
-       
+        def Klick_V():
+            Imagesz.config(image=image1)
+            label2.config(text="- Vasilisa: She is your mother's sister, and she is very kind and caring. \nShe loves to cook and bake, and she always makes delicious meals for the family. \nShe is also very creative and loves to make crafts and decorations for special occasions.")
+            
+            Imagesz.config(image=image2)
+            Imagesz.image = image2
+            Imagesz.pack( padx="10", pady="10")
+            
+
+        def Klick_M():
+            ZmnButton2 = "Masha"
+            label2.config(text="- Masha: She is your father's sister, and she is very adventurous and outgoing. \nShe loves to travel and explore new places, and she always has exciting stories to share. \nShe is also very athletic and enjoys playing sports and outdoor activities.")
+            
+            Imagesz.config(image=image3)
+            Imagesz.image = image3
+            Imagesz.pack( padx="10", pady="10")
+
+        ButtonV = Button(self.root, text="Vasilisa",command=Klick_V, font=("Lobster", 20, 'bold'), bg="red").pack(side="left", padx="10", pady="10")
+        ButtonM = Button(self.root, text="Masha", command= Klick_M,font=("Lobster", 20, 'bold'), bg="blue").pack(side="right", padx="10", pady="10")
         # Exit button
         ButtonExit = Button(self.root, text="Exit", font=("Lobster", 20, 'bold'), command=self.root.quit, bg="green").pack(side="bottom", padx="10", pady="50")
 
@@ -62,8 +88,7 @@ class FeastApp:
     
     def run(self):
             # Run the application
-            self.root.mainloop()       
-    
+            self.root.mainloop()   
 
 if __name__ == "__main__":
     app = FeastApp()
